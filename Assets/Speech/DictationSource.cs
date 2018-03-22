e5e617776d2eed4905be2447d59e9eff831ef8dd
Mike Taulty <mtaulty@microsoft.com>
2018-03-22 07:29:33 +0000

    Added a switch based on a keyword recognizer

--- Assets/Speech/DictationSource.cs
+++ Assets/Speech/DictationSource.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
 public class DictationSource : MonoBehaviour
 {
+    public event EventHandler DictationStopped;
+
     public float initialSilenceSeconds;
     public float autoSilenceSeconds;
     public LUISDictationSink dictationSink;
@@ -12,21 +13,57 @@ public class DictationSource : MonoBehaviour
     // TODO: Think about whether this should be married with the notion of
     // a focused object rather than just some 'global' entity.
 
-    void Start()
+    void NewRecognizer()
     {
         this.recognizer = new DictationRecognizer();
-        this.recognizer.InitialSilenceTimeoutSeconds = 0;
-        this.recognizer.AutoSilenceTimeoutSeconds = 0;
+        this.recognizer.InitialSilenceTimeoutSeconds = this.initialSilenceSeconds;
+        this.recognizer.AutoSilenceTimeoutSeconds = this.autoSilenceSeconds;
         this.recognizer.DictationResult += OnDictationResult;
-        this.recognizer.Start();       
+        this.recognizer.DictationError += OnDictationError;
+        this.recognizer.DictationComplete += OnDictationComplete;
+        this.recognizer.Start();
+    }
+    public void Listen()
+    {
+        this.NewRecognizer();
+    }
+    void OnDictationComplete(DictationCompletionCause cause)
+    {
+        this.FireStopped();
+    }
+    void OnDictationError(string error, int hresult)
+    {
+        this.FireStopped();
     }
     void OnDictationResult(string text, ConfidenceLevel confidence)
     {
+        this.recognizer.Stop();
+
         if ((confidence == ConfidenceLevel.Medium) ||
             (confidence == ConfidenceLevel.High) &&
             (this.dictationSink != null))
         {
             this.dictationSink.OnDictatedText(text);
+        }
+    }
+    void FireStopped()
+    {
+        this.recognizer.DictationComplete -= this.OnDictationComplete;
+        this.recognizer.DictationError -= this.OnDictationError;
+        this.recognizer.DictationResult -= this.OnDictationResult;
+        this.recognizer.Dispose();
+        this.recognizer = null;
+
+        // There's some dragons here in trying to not have a KeywordRecognizer
+        // and a DictationRecognizer active at the same time. Unity doesn't
+        // like that and this code is trying to avoid that after much
+        // experimenting. I still see Unity Assert on this line though :-(
+        // in the editor.
+        PhraseRecognitionSystem.Shutdown();
+
+        if (this.DictationStopped != null)
+        {
+            this.DictationStopped(this, EventArgs.Empty);
         }
     }
     DictationRecognizer recognizer;
---
+++ Assets/Speech/KeywordDictationSwitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class KeywordDictationSwitch : MonoBehaviour
+{
+    public string keyword = "listen";
+    public DictationSource dictationSource;
+
+    void Start()
+    {
+        this.NewRecognizer();
+        this.dictationSource.DictationStopped += this.OnDictationStopped;
+    }
+    void NewRecognizer()
+    {
+        this.recognizer = new KeywordRecognizer(new string[] { this.keyword });
+        this.recognizer.OnPhraseRecognized += this.OnPhraseRecgonized;
+        this.recognizer.Start();
+    }
+    void OnDictationStopped(object sender, System.EventArgs e)
+    {
+        this.NewRecognizer();
+    }
+    void OnPhraseRecgonized(PhraseRecognizedEventArgs args)
+    {
+        if (((args.confidence == ConfidenceLevel.Medium) ||
+            (args.confidence == ConfidenceLevel.High)) &&
+            (args.text == this.keyword) &&
+            (this.dictationSource != null))
+        {
+            // There's some dragons here in trying to not have a KeywordRecognizer
+            // and a DictationRecognizer active at the same time. Unity doesn't
+            // like that and this code is trying to avoid that after much
+            // experimenting. 
+            this.recognizer.Stop();
+            this.recognizer.OnPhraseRecognized -= this.OnPhraseRecgonized;
+            this.recognizer.Dispose();
+            this.recognizer = null;
+            PhraseRecognitionSystem.Shutdown();
+            this.dictationSource.Listen();
+        }
+    }
+    void StartDictation()
+    {
+        this.dictationSource.Listen();
+    }
+    KeywordRecognizer recognizer;
+}
